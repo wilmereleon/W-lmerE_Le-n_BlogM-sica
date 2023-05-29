d139c052c05bf578532559f3c29ebc54faa6ba1c
Anpin5432 <pineda.andres@correounivalle.edu.co>
2023-05-29 12:29:58 -0500

    Se complementa validacion

--- js/validaciones.js
+++ js/validaciones.js
@@ -10,10 +10,10 @@ enviar.addEventListener('click', validacion)
 function validacion() {
     let comentario = document.getElementById('comment')
     let parrafo = document.getElementById('validation')
-    if(comentario.value.length <= 4){
-        parrafo.innerHTML = 'Inserte comentario mas extenso'
-    }else{
+    if(comentario.value.length >= 1){
         parrafo.innerHTML = 'Su comentario ha sido enviado.'
+    }else{
+        parrafo.innerHTML = 'Inserte comentario mas extenso'
     }
 }
 
